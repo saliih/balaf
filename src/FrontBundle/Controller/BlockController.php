eac011dbe44167064ace716786d26861dd2b7e5a
saliih <salah.chtioui@gmail.com>
2016-06-06 16:08:11 +0100

    last view

--- src/FrontBundle/Controller/BlockController.php
+++ src/FrontBundle/Controller/BlockController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BlockController extends Controller
 {
+    public function gerRecords($nb){
+        return $article = $this->getDoctrine()->getRepository("PostBundle:Post")->findBy(
+            array("enabled"=>true),array('id'=>"DESC"),$nb
+        );
+    }
     public function topAction()
     {
-        $article = $this->getDoctrine()->getRepository("PostBundle:Post")->findBy(
-            array("enabled"=>true),array('id'=>"DESC")
-        );
+        $article = $this->gerRecords(5);
         return $this->render('FrontBundle:Block:top1.html.twig', array('article' => $article));
+    }
+    public function newLeftAction()
+    {
+        $article = $this->gerRecords(12);
+        return $this->render('FrontBundle:Block:newLeft.html.twig', array('article' => $article));
     }
 }
