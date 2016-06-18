b190564a1f595ad2fc013c79ba4bd4bcef202615
saliih <salah.chtioui@gmail.com>
2016-06-18 13:44:56 +0100

    index

--- src/FrontBundle/Controller/BlockController.php
+++ src/FrontBundle/Controller/BlockController.php
@@ -39,7 +39,7 @@ class BlockController extends Controller
     public function mamanAction()
     {
         $this->catid[] = 7;
-        $category = $this->getDoctrine()->getRepository("PostBundle:Category")->find(4);
+        $category = $this->getDoctrine()->getRepository("PostBundle:Category")->find(7);
         //$this->recurcive($category);
         $article = $this->getDoctrine()->getRepository("PostBundle:Post")->findBy(array("enabled" => true, 'category' => $category), array('id' => "DESC"), 5);
         return $this->render('FrontBundle:Block:maman.html.twig', array('article' => $article));
