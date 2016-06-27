babb92f04d98a63ae0e2e0929125cfa1fc9085d7
Salah <salah.chtioui@gmail.com>
2016-06-27 09:28:38 +0100

    Update ArticleController.php

--- src/FrontBundle/Controller/ArticleController.php
+++ src/FrontBundle/Controller/ArticleController.php
@@ -10,25 +10,27 @@ class ArticleController extends Controller
     public function indexAction($locale,$categoryname,$year,$month,$slug)
     {
         $request = $this->get('request');
+        $em = $this->getDoctrine()->getEntityManager();
         $session = $request->getSession();
         $pageView = $session->get('pageView');
         $article = $this->getDoctrine()->getRepository('PostBundle:Post')->findOneBy(array('alias'=>$slug));
-        if(!isset($pageView[$article->getId()])){
-            $pageView[$article->getId()] = $article->getNbview();
+        //if(!isset($pageView[$article->getId()])){
+          //  $pageView[$article->getId()] = $article->getNbview();
             $newnb = $article->getNbview() + 1;
             $article->setNbview($newnb);
-            $this->getDoctrine()->getEntityManager()->persist($article);
-            $this->getDoctrine()->getEntityManager()->flush();
-            $session->set('pageView',$pageView);
-        }
-        $related = $this->getDoctrine()->getRepository('PostBundle:Post')->findBy(array("category"=>$article->getCategory(),'enabled'=>true),array());
+            $em->persist($article);
+            $em->flush();
+            //$session->set('pageView',$pageView);
+        //}
+        $related = array();
+      /*  $related = $this->getDoctrine()->getRepository('PostBundle:Post')->findBy(array("category"=>$article->getCategory(),'enabled'=>true),array());
         shuffle($related);
         $i = 0;
         $related2 = $related;
         foreach ($related2 as $key=>$value){
             if($i>1)unset($related[$key]);
             ++$i;
-        }
+        }*/
         return $this->render('FrontBundle:Article:index.html.twig', array('article' => $article,'related'=>$related));
     }
 }
