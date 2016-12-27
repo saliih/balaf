a0e44327f60fe81e659b849a892db61369e9f535
Salah Chtioui <salah.chtioui@rayensoft.com>
2016-12-27 14:30:26 +0100

    block share

--- src/PostBundle/Block/ShareBlockService.php
+++ src/PostBundle/Block/ShareBlockService.php
@@ -51,7 +51,7 @@ class ShareBlockService extends BaseBlockService
         // merge settings
         $settings = $blockContext->getSettings();
         $posts = $this->em->getRepository('PostBundle:Post')->findBy(array('twitter' => false, 'enabled' => true), array('id' => 'DESC'),3);
-        $postsnonactive = $this->em->getRepository('PostBundle:Post')->findBy(array('twitter' => true, 'enabled' => true));
+        $postsnonactive = $this->em->getRepository('PostBundle:Post')->findBy(array('twitter' => false, 'enabled' => true));
 
         return $this->renderResponse($blockContext->getTemplate(), array(
             'posts' => $posts,
