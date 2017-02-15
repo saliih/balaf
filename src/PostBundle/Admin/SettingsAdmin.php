ab31590081cf0a1fde6eff62baeffcbe9f07b7ae
Salah Chtioui <salah.chtioui@gmail.com>
2017-02-15 15:20:29 +0100

    settings

---
+++ src/PostBundle/Admin/SettingsAdmin.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sarra
+ * Date: 15/05/16
+ * Time: 14:34
+ */
+
+namespace PostBundle\Admin;
+
+use PostBundle\Admin\BaseAdmin as Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Knp\Menu\ItemInterface as MenuItemInterface;
+
+class SettingsAdmin extends Admin
+{
+
+    public function getname()
+    {
+        return 'Settings';
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+
+            ->add('name', null, array('label' => 'Keywords'))
+            ->add('act', null, array('label' => 'traité', 'editable'=>true))
+            ->add('result')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    //'delete' => array(),
+                )
+            ));
+
+    }
+}
---
+++ src/PostBundle/Entity/Settings.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace PostBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Settings
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Settings
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="act", type="boolean")
+     */
+    private $act;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Settings
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set act
+     *
+     * @param boolean $act
+     * @return Settings
+     */
+    public function setAct($act)
+    {
+        $this->act = $act;
+
+        return $this;
+    }
+
+    /**
+     * Get act
+     *
+     * @return boolean 
+     */
+    public function getAct()
+    {
+        return $this->act;
+    }
+}
