089a879ad141a264f921db0327e7928eb2e2d809
Salah Chtioui <salah.chtioui@gmail.com>
2017-02-17 11:32:54 +0100

    ramadan 2017

--- src/FrontBundle/Controller/CategoryController.php
+++ src/FrontBundle/Controller/CategoryController.php
@@ -7,41 +7,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CategoryController extends Controller
 {
-    public function indexAction($locale,$slug)
-    {
-        $request = $this->get("request");
-        $category = $this->getDoctrine()->getRepository('PostBundle:Category')->findOneBy(array('slug'=>$slug,'locale'=>$locale));
-        if($category == null)return $this->redirect($this->generateUrl('front_homepage'));
-        $posts = $this->getDoctrine()->getRepository("PostBundle:Post")->findBy(array('category'=>$category,'enabled' => true),array('publieddate'=>'DESC', 'id'=>'DESC'));
-        $paginator  = $this->get('knp_paginator');
+    public function ramadanAction(Request $request){
+        $posts = $this->getDoctrine()->getRepository("PostBundle:Post")->findBy(array('ramadan2017' => true, 'enabled' => true), array('publieddate' => 'DESC', 'id' => 'DESC'));
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $posts, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
             12/*limit per page*/
         );
-        return $this->render('FrontBundle:Category:index.html.twig', array("category"=>$category,'posts' => $pagination));
+        return $this->render('FrontBundle:Category:ramadan.html.twig', array("category" => $category, 'posts' => $pagination));
     }
-    public function searchAction(){
+
+    public function indexAction($locale, $slug)
+    {
+        $request = $this->get("request");
+        $category = $this->getDoctrine()->getRepository('PostBundle:Category')->findOneBy(array('slug' => $slug, 'locale' => $locale));
+        if ($category == null) return $this->redirect($this->generateUrl('front_homepage'));
+        $posts = $this->getDoctrine()->getRepository("PostBundle:Post")->findBy(array('category' => $category, 'enabled' => true), array('publieddate' => 'DESC', 'id' => 'DESC'));
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $posts, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            12/*limit per page*/
+        );
+        return $this->render('FrontBundle:Category:index.html.twig', array("category" => $category, 'posts' => $pagination));
+    }
+
+    public function searchAction()
+    {
         $em = $this->getDoctrine()->getManager();
         $request = $this->get('request');
         $search = $request->query->get('s');
         $page = $request->query->get('page');
         // history search
         $posts = $this->getDoctrine()->getRepository('PostBundle:Post')->search($search);
-        if(in_array($page,array('1','',0))) {
+        if (in_array($page, array('1', '', 0))) {
             $findstr = new Search();
             $findstr->setResult(count($posts));
             $findstr->setSearch($search);
             $em->persist($findstr);
             $em->flush();
         }
-        $paginator  = $this->get('knp_paginator');
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $posts, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
             12/*limit per page*/
         );
-        return $this->render('FrontBundle:Category:search.html.twig', array("search"=>$search,'posts' => $pagination));
+        return $this->render('FrontBundle:Category:search.html.twig', array("search" => $search, 'posts' => $pagination));
 
     }
 }
