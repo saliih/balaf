bc7680da880326ad39d93e4c9ceae88d97a0f7a8
Salah Chtioui <salah.chtioui@gmail.com>
2017-03-15 15:00:09 +0100

    views

--- src/FrontBundle/Controller/ArticleController.php
+++ src/FrontBundle/Controller/ArticleController.php
@@ -14,22 +14,20 @@ class ArticleController extends Controller
         $em = $this->getDoctrine()->getEntityManager();
         $article = $this->getDoctrine()->getRepository('PostBundle:Post')->findOneBy(array('alias' => $slug));
         $ip = $request->getClientIp();
-        $view = $this->getDoctrine()->getRepository('PostBundle:Views')->findOneBy(array('post'=>$article,'ip'=>$ip));
+        $view = $this->getDoctrine()->getRepository('PostBundle:Views')->findOneBy(array('post' => $article, 'ip' => $ip));
         $newnb = $article->getNbview() + 1;
         $article->setNbview($newnb);
         $em->persist($article);
-        if($view==null) {
+        if ($view == null) {
             $view = new Views();
             $view->setPost($article);
             $view->setIp($ip);
             $view->setCreatedby($article->getCreatedby());
             $view->setRefer($request->headers->get('referer'));
-            if($view->getRefer()!="") {
-                $em->persist($view);
-            }
+            $em->persist($view);
         }
         $em->flush();
-        $related = $this->getDoctrine()->getRepository('PostBundle:Post')->findBy(array("category" => $article->getCategory(), 'enabled' => true), array('publieddate' => 'DESC', 'id'=>'DESC'));
+        $related = $this->getDoctrine()->getRepository('PostBundle:Post')->findBy(array("category" => $article->getCategory(), 'enabled' => true), array('publieddate' => 'DESC', 'id' => 'DESC'));
         shuffle($related);
         $i = 0;
         $related2 = $related;
