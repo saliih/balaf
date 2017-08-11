9c7a4d277b6ce1480bf02f5df0d7109547c51474
Salah Chtioui <salah.chtioui@gmail.com>
2017-08-11 11:44:48 +0100

    reset Backup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,7 +34,6 @@ class AppKernel extends Kernel
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new Zenstruck\BackupBundle\ZenstruckBackupBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
