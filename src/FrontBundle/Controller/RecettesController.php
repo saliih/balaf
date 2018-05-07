62200a2f42f186f142d321f547767319fe8cfeab
Salah Chtioui <salah.chtioui@gmail.com>
2018-05-07 11:25:31 +0100

    recette du jour

--- src/FrontBundle/Controller/RecettesController.php
+++ src/FrontBundle/Controller/RecettesController.php
@@ -8,8 +8,12 @@ class RecettesController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
         $dt = new \DateTime();
         $recette = $this->getDoctrine()->getRepository("PostBundle:Recettes")->findOneBy(array('datepub'=>$dt));
+        $recette->setView($recette->getView()+1);
+        $em->persist($recette);
+        $em->flush();
         return $this->render('FrontBundle:Recettes:index.html.twig', array("recette"=>$recette));
     }
 }
