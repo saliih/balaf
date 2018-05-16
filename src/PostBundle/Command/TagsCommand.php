ff8763ecd9f9436ced9425c6b4ce8b1d3a34dc14
Salah Chtioui <salah.chtioui@gmail.com>
2018-05-16 14:24:04 +0100

    tags rate

---
+++ src/PostBundle/Command/TagsCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PostBundle\Command;
+
+use PostBundle\Entity\Tags;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TagsCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('tounsia:tags')
+            ->setDescription('generate rate tags');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $tags = $this->getContainer()->get('doctrine')->getRepository("PostBundle:Tags")->findAll();
+        $total = count($tags);
+        /** @var Tags $tag */
+        foreach ($tags as $tag){
+            $nbpost = count($tag->getPost());
+            $rate = $nbpost * 10 / $total;
+            $output->writeln($tag->getName()." : ".$rate);
+            $tag->setRate($rate);
+            $em->persist($tag);
+        }
+        $em->flush();
+
+    }
+}
