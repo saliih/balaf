807c4c393f56248b399a867090e6149ed4b7144e
salah chtioui <salah.chtioui@rayensoft.com>
2018-12-10 10:41:07 +0100

    enlever les stats

--- src/FrontBundle/Controller/ArticleController.php
+++ src/FrontBundle/Controller/ArticleController.php
@@ -46,35 +46,9 @@ class ArticleController extends Controller
         $article = $this->getDoctrine()->getRepository('PostBundle:Post')->findOneBy(array('alias' => $slug));
         $ip = $request->getClientIp();
         if (!in_array($ip,array('45.58.117.232', "197.3.10.74"))) {
-            $view = $this->getDoctrine()->getRepository('PostBundle:Views')->findOneBy(array(
-                'post' => $article,
-                'ip' => $ip,
-                'dv' => new \DateTime()
-            ));
             $newnb = count($article->getView()) + 1;
-
-            $refer = $request->headers->get('referer');
-            $refer = parse_url($refer, PHP_URL_HOST);
-            if ($view === null && $refer != "" && $article->getEnabled()) {
-                $referLink = $this->getDoctrine()->getRepository('PostBundle:Refer')->findOneBy(array('title'=>$refer));
-                if ($referLink === null) {
-                    $referLink = new Refer();
-                    $referLink->setTitle($refer);
-                    $em->persist($referLink);
-                    $em->flush();
-                }
-                $view = new Views();
-                $view->setPost($article);
-                $view->setIp($ip);
-                $view->setCreatedby($article->getCreatedby());
-                $view->setRefer($refer);
-                $view->setReferLinks($referLink);
-                $detect = new MobileDetect();
-                $view->isMobile($detect->isMobile() && $detect->isTablet());
-                $em->persist($view);
-                $article->setNbview($newnb);
-                $em->persist($article);
-            }
+            $article->setNbview($newnb);
+            $em->persist($article);
             $em->flush();
         }
         $related = $this->getDoctrine()->getRepository('PostBundle:Post')->findBy(array("category" => $article->getCategory(), 'enabled' => true), array('publieddate' => 'DESC', 'id' => 'DESC'));
